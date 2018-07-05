71181ebafd749d5038ba3369e389b328b99d9d0e
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-05 12:13:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function takeANumber(line, name) {
+  var i = line.length + 1;
+  line.push(name);
+  return  'Welcome, ' + name + '. ' + 'You are number ' + i + ' in line.';
+}
+
+function nowServing(line) {
+  if (line.length == 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+      return 'Currently serving ' + line.shift() + '.';
+  }
+}
+
+function currentLine(line) {
+  if (line.length ==0) {
+    return 'The line is currently empty.';
+  } else { var i = 0;
+      return 'The line is currently: ' + 1 + '. ' + line[i] + ', ' + 2 + '. ' + line[1] + ', ' + 3 + '. ' + line[2];
+      }
+  }
