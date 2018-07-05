7256258140e71a5e0452341b28b03a5ad61ba41b
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-05 11:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-function takeANumber(katzDeliLine, name) {
-  var len = katzDeliLine.length;
-  var position = len + 1;
-  katzDeliLine.push(name);
-  return 'Welcome, ' + name + '. ' + 'You are number ' + position + ' in line.';
-}
+var katzDeli = [];
 
-function nowServing(deliLine) {
-
-  if (deliLine.length == 0) {
+function nowServing() {
+  if katzDeliLine.length == 0 {
     return 'There is nobody waiting to be served!';
   } else {
-      return 'Currently serving ' + deliLine[0] + '.';
-      deliLine = deliLine.slice(1);
-      return deliLine;
+      return 'Now serving ' + katzDeliLine[0] + '!';
+      katzDeliLine = katzDeliLine.splice(1);
   }
 }
-
-function currentLine(line) {
-  if (line.length !=0) {
-    
-    return 'The line is currently: '
-    }
-  }
