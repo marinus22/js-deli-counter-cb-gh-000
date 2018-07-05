470496e61e8e92d768815b956cee8ea630807ade
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-05 11:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function takeANumber(line, name) {
   return  'Welcome, ' + name + '. ' + 'You are number ' + i + ' in line.';
 }
 
-function nowServing() {
-  if (katzDeliLine.length == 0) {
+function nowServing(line) {
+  if (line.length == 0) {
     return 'There is nobody waiting to be served!';
   } else {
-      return 'Currently serving ' + katzDeliLine[0] + '.';
-      katzDeliLine = katzDeliLine.splice(1);
+      return 'Currently serving ' + line[0] + '.';
+      return line = line.splice(1);
   }
 }
 
@@ -19,6 +19,6 @@ function currentLine(line) {
   if (line.length ==0) {
     return 'The line is currently empty.';
   } else {
-      
+
   }
 }
