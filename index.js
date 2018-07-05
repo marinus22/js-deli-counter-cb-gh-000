6e11c6328bc578b197e99588fcd2a9c6006f2d68
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-05 12:12:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function currentLine(line) {
   if (line.length ==0) {
     return 'The line is currently empty.';
   } else { var i = 0;
-      return 'The line is currently: ' + 1 + '. ' + line[i] + ', ' + 2 + '. ' + line[i++] + ', ' + 3 + '. ' + line[2];
+      return 'The line is currently: ' + 1 + '. ' + line[i] + ', ' + 2 + '. ' + line[1] + ', ' + 3 + '. ' + line[2];
       }
   }
